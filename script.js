dfe0655c835cfa1701807beac9de8ab87070fba9
chicotorino <166443027+chicotorino@users.noreply.github.com>
2024-06-17 19:11:55 -0300

    Add files via upload
    Update Script

--- script.js
+++ script.js
@@ -6,16 +6,23 @@
         var tarjetas = document.getElementsByClassName("card");
         for(var i = 0; i < tarjetas.length; i++)
         tarjetas[i].className += " dark";
+        var acordeon = document.getElementsByClassName("accordion-body");
+        for(var i = 0; i < acordeon.length; i++)
+        acordeon[i].className += " dark";
+        
     }
     else {
         document.body.classList.remove("dark");
         var tarjetas = document.getElementsByClassName("card");
         for(var i = 0; i < tarjetas.length; i++)
         tarjetas[i].className -= " dark";
+        var acordeon = document.getElementsByClassName("accordion-body");
+        for(var i = 0; i < acordeon.length; i++)
+        acordeon[i].className -= " dark";
         
     }
 
-    // Guardamos el modo en localstorage.
+    // Guardamos el modo en localStorage.
 
     if (document.body.classList.contains("dark")){
         localStorage.setItem("dark-mode", "true");
@@ -31,6 +38,10 @@ if(localStorage.getItem("dark-mode") === "true"){
     var tarjetas = document.getElementsByClassName("card");
     for(var i = 0; i < tarjetas.length; i++)
     tarjetas[i].className += " dark";
+    var acordeon = document.getElementsByClassName("accordion-body");
+    for(var i = 0; i < acordeon.length; i++)
+    acordeon[i].className += " dark";
+
     document.getElementById('input').checked=true;
 
 }   else {
@@ -38,6 +49,10 @@ if(localStorage.getItem("dark-mode") === "true"){
     var tarjetas = document.getElementsByClassName("card");
     for(var i = 0; i < tarjetas.length; i++)
     tarjetas[i].className -= " dark";
+    var acordeon = document.getElementsByClassName("accordion-body");
+    for(var i = 0; i < acordeon.length; i++)
+    acordeon[i].className -= " dark";
+
     document.getElementById('input').checked=false;
 }
 
